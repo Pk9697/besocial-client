de11446fccd351fc32f7660db7b9916fa36f3afa
Prashant Kumar <prashant.aman9@gmail.com>
2023-03-13 22:20:47 +0530

    add Login comp as controlled component for input fields

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Login() {
+    // const [email, setEmail] = useState("")
+    // const [password, setPassword] = useState("")
+    const [formFields,setFormFields]=useState({
+        email:"",
+        password:""
+    })
+
+    function handleChange(e){
+        const {name,value} =e.target
+        // console.log(name,value)
+        setFormFields((prev)=>{
+            return {
+                ...prev,
+                [name]:value
+            }
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        setFormFields(()=>{
+            return {
+                email:"",
+                password:""
+            }
+        })
+    }
+    console.log(formFields)
   return (
     <div className="login">
-        <div className='widget-wrapper mw-700 login-wrapper'>
-            <input className='login__input' type="email" placeholder='Email' />
-            <input className='login__input' type="password" placeholder='Password' />
-            <button className='login__input login__btn'>LOGIN</button>
-        </div>
+        <form onSubmit={handleSubmit} className='widget-wrapper mw-700 login-wrapper'>
+            <input name='email' value={formFields.email} onChange={handleChange} className='login__input' type="email" placeholder='Email' required/>
+            <input name='password' value={formFields.password} onChange={handleChange} className='login__input' type="password" placeholder='Password' required/>
+            <button type='submit' className='login__input login__btn'>LOGIN</button>
+        </form>
     </div>
   )
 }

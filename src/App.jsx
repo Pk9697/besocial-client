c349bde0410fb8721336e58107feaadb4d87dc00
Prashant Kumar <prashant.aman9@gmail.com>
2023-03-13 21:08:15 +0530

    fix user image shrink using flex-shrink

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 function App() {
   return (
-    <div className='app'>
-        <Router>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/register' element={<Register/>} />
-          </Routes>
-        </Router>
-    </div>
-  )
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

38fb66e6e128c3c4c45028e0b47cdd413b2ad6c4
Prashant Kumar <prashant.aman9@gmail.com>
2023-03-14 21:52:49 +0530

    add alert functionality to inform user if login error happens

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {useDispatch,useSelector} from "react-redux"
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { login, startLogin } from '../actions/auth'
 function Login() {
     // const [email, setEmail] = useState("")
@@ -8,6 +9,7 @@ function Login() {
         email:"",
         password:""
     })
+    const [isAlertClosed, setIsAlertClosed] = useState(false)
     const dispatch=useDispatch()
     const auth=useSelector(state=>state.auth)
     // console.log(auth)
@@ -31,6 +33,7 @@ function Login() {
                 password:""
             }
         })
+        setIsAlertClosed(false)
     }
     // console.log(formFields)
   return (
@@ -40,6 +43,16 @@ function Login() {
             <input name='password' value={formFields.password} onChange={handleChange} className='login__input' type="password" placeholder='Password' required/>
             <button disabled={auth.inProgress} type='submit' className='login__input login__btn'>{auth.inProgress?'LOGGING IN':'LOG IN'}</button>
         </form>
+        {auth.error && !isAlertClosed &&
+            <div className='widget-wrapper mw-700 alert'>
+                <div className='flex-row'>
+                    <p>{auth.error}</p>
+                    <div className='icon' onClick={()=>setIsAlertClosed(true)}> 
+                        <CloseOutlinedIcon/>
+                    </div>
+                </div>
+            </div>
+        }
     </div>
   )
 }

aca269024366c38f6ed26f80988995885bf13a43
Prashant Kumar <prashant.aman9@gmail.com>
2023-03-17 21:02:35 +0530

    fix design of homepage when not logged in and add max-width of grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 import FriendsList from '../components/FriendsList'
 import PostForm from '../components/PostForm'
 import PostsList from '../components/PostsList'
@@ -6,14 +7,20 @@ import UserWidget from '../components/UserWidget'
 import UsersListWidget from '../components/UsersListWidget'
 
 function Home() {
+  const auth=useSelector(state=>state.auth)
+  const {isLoggedIn}=auth
   return (
     <div>
-        <div className='grid'>
-            <UserWidget/>
-            <FriendsList/>
+        <div className={!isLoggedIn?'grid-notloggedin':'grid'}>
+            {isLoggedIn && 
+              <>
+                <UserWidget/>
+                <FriendsList/>
+              </>
+            }
             <UsersListWidget/>
           <section className='flex-col grid__section'>
-            <PostForm/>
+            {isLoggedIn && <PostForm/>}
             <PostsList/>
           </section>
         </div>

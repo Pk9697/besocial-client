9fc6538e1b9246f1ae9c315481a6f5bb0b4136cf
Prashant Kumar <prashant.aman9@gmail.com>
2023-03-29 20:48:55 +0530

    Separate Post as a component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,82 +1,20 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined'
-import PersonRemoveOutlinedIcon from '@mui/icons-material/PersonRemoveOutlined'
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
 import { fetchPosts } from '../actions/posts'
-import { addFriend, removeFriend } from '../actions/friends'
+import Post from './Post'
+
 function PostsList() {
 	const dispatch = useDispatch()
 	const posts = useSelector((state) => state.posts)
-	const auth = useSelector((state) => state.auth)
-	const friends = useSelector((state) => state.friends)
-	const { friendsArr } = friends
 
 	useEffect(() => {
 		dispatch(fetchPosts())
 	}, [])
 
-	function isFriend(userId) {
-		return Boolean(friendsArr.find((friend) => friend.to_user._id === userId))
-	}
-
-	function isLoggedInUser(userId) {
-		return userId === auth.user._id
-	}
-
 	return (
 		<div className='postslist-widget posts-container flex-col'>
 			{posts.map((post) => (
-				<div key={post._id} className='widget-wrapper'>
-					<div className='user'>
-						<Link to={`/profile/${post.user._id}`} className='user'>
-							<img
-								className='user__img'
-								src={`http://localhost:4001${post.user.avatar}`}
-								alt='user_pic'
-							/>
-							<h5 className='user__name'>{post.user.name}</h5>
-						</Link>
-						{!isLoggedInUser(post.user._id) ? (
-							isFriend(post.user._id) ? (
-								<div
-									className='user__icon icon'
-									onClick={() =>
-										dispatch(removeFriend(post.user._id, auth.token))
-									}
-								>
-									<PersonRemoveOutlinedIcon fontSize='small' />
-								</div>
-							) : (
-								<div
-									className='user__icon icon'
-									onClick={() => dispatch(addFriend(post.user._id, auth.token))}
-								>
-									<PersonAddAlt1OutlinedIcon fontSize='small' />
-								</div>
-							)
-						) : (
-							<></>
-						)}
-					</div>
-					<p className='post__content'>{post.content}</p>
-					<div className='post__interactions'>
-						<div className='post__likes'>
-							<div className='icon'>
-								<FavoriteBorderOutlinedIcon />
-							</div>
-							<p>{post.likes.length}</p>
-						</div>
-						<div className='post__comments'>
-							<div className='icon'>
-								<ChatBubbleOutlineOutlinedIcon />
-							</div>
-							<p>{post.comments.length}</p>
-						</div>
-					</div>
-				</div>
+				<Post key={post._id} post={post} dispatch={dispatch} />
 			))}
 		</div>
 	)
